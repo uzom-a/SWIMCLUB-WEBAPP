983acc498df7c2448f63c984aafb1d9d705cd5af
Uzoma Stephanie-Portia Okey-Anyanwu <166033308+uzom-a@users.noreply.github.com>
2024-09-25 22:22:16 -0500

    updates

---
+++ webapp/app copy.py
@@ -0,0 +1,66 @@
+import os
+import swimclub
+
+
+from flask import Flask, session,render_template,request
+
+app = Flask(__name__)
+app.secret_key = "You will never guess"
+
+FOLDER = "swimdata/"
+
+def populate_data():
+    if "swimmers" not in session: #this ensures previous data does not enter again
+     swim_files = os.listdir(swimclub.FOLDER) 
+     swim_files.remove(".DS_Store")
+     session["swimmers"] = {}
+     for file in swim_files: # loop through each file
+        name, *_ = swimclub.read_swim_data(file) # read swimmer names from file
+        if name not in session["swimmers"]: # check if names are already in the list
+           session["swimmers"][name] = []
+        session["swimmers"][name].append(file)
+    #this function does not need to return anything
+
+@app.get("/") #this takes you to the default web page
+def index():
+    return render_template(#there is no need to specify the templates folder here because the imported function will check that by default.
+       "index.html",
+       title="Welcome to the Swimclub webapp!!",
+       )
+@app.get("/swimmers") #this takes you to the swimmers website 
+def display_swimmers():
+    populate_data()
+    return render_template(
+        "select.html",
+        title="Choose a swimmer",
+        select_id="swimmer",
+        url="/showfiles",
+        data=sorted(session["swimmers"]),
+    )
+
+@app.get("/files/<swimmer>")
+def get_swimmers_files(swimmer):
+    populate_data()
+    return str(session["swimmers"][swimmer])
+
+@app.post("/showfiles")#you need this because the select.html uses POST
+def display_swimmer_files():
+        populate_data()
+        name = request.form["swimmer"] #this stores the swimmers name from the select_id key(swimmer)
+        return render_template(
+            "select.html",
+            title="Select an event", 
+            url="/showbarchart",
+            select_id = "file",#give the <select> tag a name.
+            data=session["swimmers"][name],#pass in the data needed for the next drop-down list which in this case is the list of filenames associated with each swimmer
+        )
+
+@app.post("/showbarchart")
+def show_bar_chart():
+    file_id = request.form["file"]#Grab the value of the "file"select_id from the html form
+    location = swimclub.produce_bar_chart(file_id, "templates/" )#this takes in the filename and changes the location where the data is saved
+    return render_template( location.split("/")[-1] )#the -1 is an index that helps you select data from the end of the list 
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
---
+++ webapp/swimclub.py
@@ -0,0 +1,61 @@
+import statistics #helps us find the average
+import hfpy_utils #this is a open source module
+import webbrowser
+import os
+
+CHARTS = "charts/"
+
+FOLDER = "swimdata/"
+
+def read_swim_data(filename):    
+    swimmer, age, distance, stroke = filename.removesuffix(".txt").split("-")
+    with open(FOLDER+filename) as df:
+        times = df.readlines()[0].strip().split(",")
+    converts=[]
+    for t in times:
+        if ":" in t:
+         minutes, rest = t.split(":")
+         seconds, hundredths = rest.split(".")
+        else:
+           minutes = 0
+           seconds, hundredths = t.split(".")
+        converts.append(int(minutes)*60*100 +int(seconds)*100 + int(hundredths) )
+    average = statistics.mean(converts)
+    mins_secs, hundredths = f"{(average / 100):.2f}".split(".")
+    mins = int(mins_secs)
+    minutes= mins // 60
+    second = mins - minutes*60
+    average = f"{minutes}:{seconds:0>2}.{hundredths}"
+    return swimmer, age, distance, stroke, times, average, converts 
+
+def produce_bar_chart(fn, location=CHARTS):
+    #Given the name of a swimmer's file, produce a HTML/SVG-based bar chart and save the chart to the the CHARTS folder and reeturn the bar chart to the bar chart file
+    swimmer, age, distance, stroke, times, average, converts = read_swim_data(fn)
+    from_max = max(converts)
+    times.reverse()
+    converts.reverse()
+    title = f"{swimmer} (Under {age}) {distance} {stroke}"
+    header = f"""<!DOCTYPE html>
+                   <html>
+                      <head>
+                           <title>{title}</title>
+                           </head>
+                           <body>
+                               <h3>{title}</h3>"""
+    body=""
+    for n, t in enumerate(times):
+       bar_width = hfpy_utils.convert2range(converts[n], 0, from_max, 0, 350)
+       body= body +f"""
+                            <svg height="30" width="400">
+                                <rect height="30" width="{bar_width}" style="fill:rgb(0,0,255);" />
+                            </svg>{t}<br />"""
+       footer = f"""
+                     <p>Average time:{average}</p>
+                  </body>
+               </html>"""
+       page = header + body + footer
+       save_to = f"{location}{fn.removesuffix('.txt')}.html"
+       with open(save_to, "w") as sf:
+            print(page, file=sf)
+
+    return save_to  
